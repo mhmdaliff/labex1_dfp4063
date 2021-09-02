23a054166bcb86b3086123267c0f0ca92ae97c9f
mhmdaliff <aliffmuhammad640@gmail.com>
2021-09-02 16:45:53 +0800

    pertama kali push

--- index.php
+++ index.php
@@ -1,26 +1,23 @@
-<html style="background-color:powderblue;">
+<html>
 
 <head></head>
 
 <body>
 
-<?php
+    <?php
     $name = 'Sherlock Holmes';
     echo "<h1>$name</h1>";
     $release = 'October 14, 1892';
-    echo "<h2>$release</h2>";
+    echo "<h1>$release</h1>";
     $author = "Sir Arthur Conan Doyle";
-    echo "<h3>$author</h3>";
+    echo "<h1>$author</h1>";
     ?>
+
+    <li><a href="about.php">About</a></li>
     <img src="cover.jpg">
 
-    <p><a href="chapter1.php">1. A Scandal in Bohemia</a></p>
-    <p><a href="chapter2.php">2. The Red-Headed League</a></p>
-    <p><a href="chapter3.php">3. A Case of Identity</a></p>
-    <p><a href="chapter4.php">4. The Boscombe Valley Mystery</a></p>
-    <p><a href="chapter5.php">5. The Five Orange Pips</a></p>
 
-   
+
 </body>
 
 </html>
