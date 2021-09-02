1cd061919668a2ab5970494fc8c26093d67a4961
mhmdaliff <aliffmuhammad640@gmail.com>
2021-09-02 10:43:20 +0800

    pertama kali push

---
+++ index.php
@@ -0,0 +1,17 @@
+<html style="background-color:powderblue;">
+
+<head><h1 font-size: 50px>eBookstore</h1></head>
+
+<body>
+<h2>Sherlock Holmes</h2>
+<h3>Chapters</h3>
+
+<p><a href="chapter1.php">1. A Scandal in Bohemia</a></p>
+<p><a href="chapter2.php">2. The Red-Headed League</a></p>
+<p><a href="chapter3.php">3. A Case of Identity</a></p>
+<p><a href="chapter4.php">4. The Boscombe Valley Mystery</a></p>
+<p><a href="chapter5.php">5. The Five Orange Pips</a></p>
+
+</body>
+
+</html>
