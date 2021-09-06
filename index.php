9dab77d0864b4b9e341e84f89d16d9f3789a741b
mhmdaliff <aliffmuhammad640@gmail.com>
2021-09-06 14:22:11 +0800

    pertama kali push

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sherlock Holmes Book</title>
-    <link rel="stylesheet" href="menu.php">
     <link rel="stylesheet" href="form.css">
 </head>
 
@@ -16,9 +15,9 @@
     $name = 'Sherlock Holmes';
     echo "<h1>$name</h1>";
     $release = 'October 14, 1892';
-    echo "<h1>$release</h1>";
+    echo "<h2>$release</h2>";
     $author = "Sir Arthur Conan Doyle";
-    echo "<h1>$author</h1>";
+    echo "<h2>$author</h2>";
     ?>
     <img src="cover.jpg">
 </body>
