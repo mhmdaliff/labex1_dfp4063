c4701e0b913ebe9dfb3b3118e781213cb246cd93
mhmdaliff <aliffmuhammad640@gmail.com>
2021-09-06 12:47:15 +0800

    pertama kali push

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sherlock Holmes Book</title>
+    <link rel="stylesheet" href="menu.php">
     <link rel="stylesheet" href="form.css">
 </head>
 
