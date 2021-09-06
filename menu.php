8cce8090be4af38c3b1145c1956586fc63c5c72d
mhmdaliff <aliffmuhammad640@gmail.com>
2021-09-06 12:52:17 +0800

    pertama kali push

--- menu.php
+++ menu.php
@@ -1,20 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Menu</title>
-    <link rel="stylesheet" href="form.css">
-</head>
-
-<body>
-    <div class="topnav">
-        <a class="active" href="index.php">Home</a>
-        <a href="about.php">About Author</a>
-        <a href="author.php">Author Biodata</a>
-    </div>
-</body>
-
-</html>
+<div class="topnav">
+    <a class="active" href="index.php">Home</a>
+    <a href="about.php">About Author</a>
+    <a href="author.php">Author Biodata</a>
+</div>
